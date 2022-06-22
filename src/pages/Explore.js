0f65cfc0084e5967638b62d83a370bb8cb26543a
Igor Castro <ncastro.igor@gmail.com>
2022-06-22 19:02:33 -0300

    Requisitos da página Explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -7,17 +7,58 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
-  const { setTitle, setIconShow } = useContext(RecipesContext);
+  const { title, setTitle, setIconShow } = useContext(RecipesContext);
   const history = useHistory();
+  const pageTitle = 'Explore Foods';
+
   useEffect(() => {
     setIconShow({ iconShow: false });
     if (history.location.pathname === '/explore/foods') {
-      setTitle({ title: 'Explore Foods' });
+      setTitle({ title: pageTitle });
     } else setTitle({ title: 'Explore Drinks' });
   }, []);
+
+  const redirecIngredient = () => {
+    if (title.title === pageTitle) {
+      history.push('/explore/foods/ingredients');
+    } else history.push('/explore/drinks/ingredients');
+  };
+
+  const redirecNationality = () => {
+    history.push('/explore/foods/nationalities');
+  };
+
+  const redirectUser = () => {
+
+  };
+
   return (
     <section className="">
       <Header />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ redirecIngredient }
+      >
+        By Ingredient
+      </button>
+      { title.title === pageTitle
+        && (
+          <button
+            type="button"
+            data-testid="explore-by-nationality"
+            onClick={ redirecNationality }
+          >
+            By Nationality
+          </button>
+        )}
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ redirectUser }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </section>
   );
--- src/pages/ExploreMain.js
+++ src/pages/ExploreMain.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import RecipesContext from '../context/Context';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,13 +8,30 @@ import Footer from '../components/Footer';
 
 function ExploreMain() {
   const { setTitle, setIconShow } = useContext(RecipesContext);
+  const history = useHistory();
+
   useEffect(() => {
     setTitle({ title: 'Explore' });
     setIconShow({ iconShow: false });
   }, []);
+
   return (
     <section className="">
       <Header />
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ () => { history.push('/explore/foods'); } }
+      >
+        Explore Foods
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => { history.push('/explore/drinks'); } }
+      >
+        Explore Drinks
+      </button>
       <Footer />
     </section>
   );
